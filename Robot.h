f756fe4155f60a62fe9d391480e6a30569691949
adir shaban <adirshaban@github.com>
2017-07-31 20:07:42 +0300

    init commit

---
+++ Robot.h
@@ -0,0 +1,36 @@
+#ifndef ROBOT_H_
+#define ROBOT_H_
+
+#include <HamsterAPIClientCPP/Hamster.h>
+#include <set>
+#include <math.h>
+#include "NodeMap/Node.h"
+#include "NodeMap/NodeMap.h"
+#include "Structs.h"
+#include "Gui/MapDrawer.h"
+#include "Constants.h"
+#include "Utils/AngleUtils.h"
+#include "Localization/LocalizationManager.h"
+
+using namespace HamsterAPI;
+using namespace std;
+
+class Robot {
+private:
+	Hamster * hamster;
+	double hamsterStartX, hamsterStartY;
+	LocalizationManager * localizationManager;
+	double resolution;
+	double mapHeight, mapWidth;
+
+public:
+	positionState prevBeliefedLocation, currBeliefedLocation, realLocation;
+
+	Robot(Hamster * hamster, LocalizationManager * localizationManager, double mapHeight, double mapWidth, double resolution);
+	positionState GetRealHamsterLocation();
+	void updateHamsterRealLocation();
+	void setHamsterSpeed(double speed, double wheelsAngle);
+	virtual ~Robot();
+};
+
+#endif /* ROBOT_H_ */
