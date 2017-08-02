88755579ae985b08a43abe003be134c949b98ace
nesh4 <nathan.zerbib@gmail.com>
2017-08-02 22:45:31 +0300

    Create LocalizationManager.h

---
+++ Localization/LocalizationManager.h
@@ -0,0 +1,18 @@
+#ifndef LOCALIZATION_MANAGER_H
+#define LOCALIZATION_MANAGER_H
+#include "Particle.h"
+#include <HamsterAPIClientCPP/Hamster.h>
+#include <list>
+using namespace std;
+using namespace HamsterAPI;
+
+class LocalizationManager
+{
+	public:
+	  std::list<Particle*> listOfParticle;
+	  LocalizationManager();
+	  void updateParticles(int deltaX, int deltaY, int deltaYaw, vector<double> sensorRead, LidarScan lidarScan);
+  
+};
+
+#endif
